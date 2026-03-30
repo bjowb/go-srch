6184b008c6051f54deff075f9f5fcf6434e4eff3
bjowb <pulktchhr@gmail.com>
2026-03-30 17:12:26 +0530

    Stage 2: Achieved

--- main.go
+++ main.go
@@ -2,78 +2,114 @@ package main
 
 import (
 	"fmt"
-	"golang.org/x/net/html"
-	//"io"
 	"log"
 	"net/http"
 	"net/url"
+
+	"golang.org/x/net/html"
 )
 
-// This is the Node type used in DOM created by golang.org/x/net/html
-//type Node struct {
-//	Type        html.Attribute
-//	Data        string
-//	Attr        []html.Attribute
-//	FirstChild  *Node
-//	NextSibling *Node
-//}
+type CrawlNode struct {
+	URL   string
+	Depth int
+}
 
 func resolveURL(href string, baseURL *url.URL) string {
 	parsedHref, err := url.Parse(href)
 	if err != nil {
 		return ""
 	}
-
 	absoluteURL := baseURL.ResolveReference(parsedHref)
-
 	return absoluteURL.String()
 }
 
-func extractLinks(node *html.Node, base *url.URL) {
-	if node.Type == html.ElementNode && node.Data == "a" {
-		for _, attr := range node.Attr {
-			if attr.Key == "href" {
-				s := resolveURL(attr.Val, base)
-				fmt.Println(s)
-				break
+func extractLinks(node *html.Node, base *url.URL) []string {
+	var links []string
+	var dfs func(*html.Node)
+	dfs = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, attr := range n.Attr {
+				if attr.Key == "href" {
+					s := resolveURL(attr.Val, base)
+					if s != "" {
+						links = append(links, s)
+					}
+					break
+				}
 			}
 		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			dfs(c)
+		}
 	}
-
-	for c := node.FirstChild; c != nil; c = c.NextSibling {
-		extractLinks(c, base)
-	}
+	dfs(node)
+	return links
 }
 
 func main() {
-	//initial seed
-	const s = "https://cppreference.com/"
-	baseUrl, err := url.Parse(s)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	fmt.Println(s, " : is the given seed by user")
 
-	//http get request to get html,css,javascript files
-	resp, err := http.Get(s)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	var initialSeeds []string
+	initialSeeds = append(initialSeeds, "https://cppreference.com/")
+	initialSeeds = append(initialSeeds, "https://wiki.archlinux.org/")
 
-	//priting to debug
-	defer resp.Body.Close()
-	//body, err := io.ReadAll(resp.Body)
-	//if err != nil {
-	//	log.Fatalln(err)
-	//}
-	//log.Println(string(body))
+	//make a queue of string
+	var queue []CrawlNode
 
-	// convert html file to DOM
-	doc, err := html.Parse(resp.Body)
-	if err != nil {
-		log.Fatalln(err)
+	//make a visisted map
+	vis := make(map[string]bool)
+
+	//initial seeds
+	for _, seeds := range initialSeeds {
+		vis[seeds] = true
+		queue = append(queue, CrawlNode{seeds, 0})
 	}
 
-	extractLinks(doc, baseUrl)
+	//Traverse the queue
+	for len(queue) > 0 {
 
+		//Deque elements
+		s := queue[0].URL
+		deep := queue[0].Depth
+		fmt.Println("Extracted Element from queue : ", s)
+		queue = queue[1:]
+
+		// if depth >= 3 then skip the link
+		if deep == 3 {
+			continue
+		}
+
+		//Parse Base URL
+		baseURL, err := url.Parse(s)
+		if err != nil {
+			log.Println("Invalid URL :", s)
+			continue
+		}
+		fmt.Println(s, " : is the given seed by user")
+
+		//Request html code
+		resp, err := http.Get(s)
+		if err != nil {
+			log.Println("Failed to fetch : ", s)
+			continue
+		}
+
+		//Parse html into DOM tree
+		doc, err := html.Parse(resp.Body)
+		if err != nil {
+			log.Println("Failed to parse HTML code for :", s)
+			resp.Body.Close()
+			continue
+		}
+
+		resp.Body.Close()
+
+		pageLinks := extractLinks(doc, baseURL)
+
+		for _, link := range pageLinks {
+			if !vis[link] {
+				vis[link] = true
+				queue = append(queue, CrawlNode{link, deep + 1})
+			}
+		}
+	}
 }
