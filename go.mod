96d046a324e530d510064cb5d45b211cbe7570a1
bjowb <pulktchhr@gmail.com>
2026-04-03 15:53:05 +0530

    Actually extracted the text and stored in SQLite DB with FTS5 fir Inverted Indices.

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go-srch
 
 go 1.26.1
 
-require golang.org/x/net v0.52.0 // indirect
+require (
+	github.com/mattn/go-sqlite3 v1.14.39
+	golang.org/x/net v0.52.0
+)
--- main.go
+++ main.go
@@ -1,18 +1,84 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
 
+	_ "github.com/mattn/go-sqlite3"
 	"golang.org/x/net/html"
 )
+
+type SearchDocument struct {
+	URL       string
+	Domain    string
+	Title     string
+	Content   string
+	Depth     int
+	Timestamp int64
+}
 
 type CrawlNode struct {
 	URL   string
 	Depth int
+}
+
+//-----------------------DATABASE FUNCTIONS-----------------------------
+
+func initDB(filepath string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", filepath)
+	if err != nil {
+		return nil, err
+	}
+
+	createTableSQL := `
+	CREATE VIRTUAL TABLE IF NOT EXISTS search_index USING fts5(
+		url UNINDEXED,
+		domain,
+		title,
+		content,
+		depth UNINDEXED,
+		timestamp UNINDEXED
+	);
+	`
+
+	_, err1 := db.Exec(createTableSQL)
+	return db, err1
+}
+
+func saveDocument(db *sql.DB, doc SearchDocument) error {
+	query := `INSERT INTO search_index (url,domain,title,content,depth,timestamp) VALUES (?,?,?,?,?,?)`
+	_, err := db.Exec(query, doc.URL, doc.Domain, doc.Title, doc.Content, doc.Depth, doc.Timestamp)
+	return err
+}
+
+//-------------------------PARSING FUNCTIONS---------------------------
+
+func extractText(node *html.Node) string {
+	if node.Type == html.TextNode {
+		cleanText := strings.TrimSpace(node.Data)
+		if cleanText != "" {
+			return cleanText + " "
+		}
+		return ""
+	}
+
+	if node.Type == html.ElementNode {
+		if node.Data == "script" || node.Data == "style" || node.Data == "noscript" {
+			return ""
+		}
+	}
+
+	var ans strings.Builder
+	for c := node.FirstChild; c != nil; c = c.NextSibling {
+		ans.WriteString(extractText(c))
+	}
+
+	return ans.String()
 }
 
 func resolveURL(href string, baseURL *url.URL) string {
@@ -47,8 +113,36 @@ func extractLinks(node *html.Node, base *url.URL) []string {
 	return links
 }
 
+func isGarbageURL(link string) bool {
+	// A list of URL substrings that indicate useless Wiki pages
+	badPaths := []string{
+		"action=edit",
+		"action=history",
+		"action=info",
+		"oldid=",
+		"diff=",
+		"Special:",
+		"User:",
+		"User_talk:",
+		"Talk:",
+		"Category:",
+		"Help:",
+		// Optional: Block non-English wiki translations (common ArchWiki pattern)
+		"(Espa", "(Fran", "(Portugu", "(Magyar)", "(Hrvatski)", "(Italiano)",
+		"(Svenska)", "(Suomi)", "(Polski)", "(%D", // %D blocks most Cyrillic/Arabic URL encodings
+	}
+
+	for _, bad := range badPaths {
+		if strings.Contains(link, bad) {
+			return true // It contains garbage, throw it away
+		}
+	}
+	return false // It looks like a clean, useful article
+}
+
+// -----------------------------------------ENGINE-----------------------------------
 // go concurrent function
-func worker(id int, jobs <-chan CrawlNode, results chan<- []CrawlNode, allowed map[string]bool) {
+func worker(id int, jobs <-chan CrawlNode, results chan<- []CrawlNode, allowed map[string]bool, db *sql.DB) {
 	for node := range jobs {
 		fmt.Println("Worker ", id, " Fetching :", node.URL)
 
@@ -79,6 +173,25 @@ func worker(id int, jobs <-chan CrawlNode, results chan<- []CrawlNode, allowed m
 
 		resp.Body.Close()
 
+		pageText := extractText(doc)
+		if len(pageText) > 100 {
+			docRecord := SearchDocument{
+				URL:       node.URL,
+				Domain:    baseURL.Hostname(),
+				Title:     "Unknown",
+				Content:   pageText,
+				Depth:     node.Depth,
+				Timestamp: time.Now().Unix(),
+			}
+
+			err = saveDocument(db, docRecord)
+			if err != nil {
+				log.Println("Database error : ", err)
+			} else {
+				fmt.Printf("[Worker %d] SAved %d chars from %s\n", id, len(pageText), node.URL)
+			}
+		}
+
 		pageLinks := extractLinks(doc, baseURL)
 		var nextNodes []CrawlNode
 		for _, link := range pageLinks {
@@ -101,34 +214,16 @@ func worker(id int, jobs <-chan CrawlNode, results chan<- []CrawlNode, allowed m
 		results <- nextNodes
 	}
 }
-func isGarbageURL(link string) bool {
-	// A list of URL substrings that indicate useless Wiki pages
-	badPaths := []string{
-		"action=edit",
-		"action=history",
-		"action=info",
-		"oldid=",
-		"diff=",
-		"Special:",
-		"User:",
-		"User_talk:",
-		"Talk:",
-		"Category:",
-		"Help:",
-		// Optional: Block non-English wiki translations (common ArchWiki pattern)
-		"(Espa", "(Fran", "(Portugu", "(Magyar)", "(Hrvatski)", "(Italiano)",
-		"(Svenska)", "(Suomi)", "(Polski)", "(%D", // %D blocks most Cyrillic/Arabic URL encodings
-	}
-
-	for _, bad := range badPaths {
-		if strings.Contains(link, bad) {
-			return true // It contains garbage, throw it away
-		}
-	}
-	return false // It looks like a clean, useful article
-}
 
 func main() {
+
+	//-------------INITIALIZE DATABASE----------
+	db, err := initDB("./search.db")
+	if err != nil {
+		log.Fatal("FAiled to initialize db :", err)
+	}
+
+	defer db.Close()
 
 	var initialSeeds []string
 	initialSeeds = append(initialSeeds, "https://cppreference.com/")
@@ -147,7 +242,7 @@ func main() {
 
 	//make 5 go-routines
 	for i := 1; i < 6; i++ {
-		go worker(i, jobs, results, allowedDomains)
+		go worker(i, jobs, results, allowedDomains, db)
 	}
 
 	//counter for number of active jobs to handle concurrency
